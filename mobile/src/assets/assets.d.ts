c0dae5f46f12095cd85924708408279da77bec4c
Ana Paula Fagundes Gomes <paulinha0939@gmail.com>
2023-05-19 20:07:06 -0300

    Aula 2

---
+++ mobile/src/assets/assets.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png' // All with PNG can be imported now
+
+declare module '*.svg' {
+  import React from 'react'
+  import { SvgProps } from 'react-native-svg'
+  const content: React.FC<SvgProps>
+  export default content
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,14 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.register(cors, {
+  origin: true, // All URLs are allowed
 })
+
+app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP Server running on port 3333')
